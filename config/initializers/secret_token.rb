8973015a1bab44aa7620fc76ef165242b8255e48
Bui The Hoa <buithehoa@gmail.com>
2014-03-06 11:46:04 +0700

    Add a blank Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Darkseid::Application.config.secret_key_base = '35fadf182bf26f39d75c66b6f96bcf9cceb5568f49123dcddb13769fa9eeee19fb10bd3ff6ae8c6873a4102b1783ec928d25a69c09caa8f4c8f7925864b69d00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Darkseid::Application.config.session_store :cookie_store, key: '_darkseid_session'
